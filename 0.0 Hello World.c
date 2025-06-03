cc8c5039673d389906354953a9771019bda496d6
zylyunlei-CIPON <zylyunlei@gmail.com>
2025-06-03 15:36:38 +0800

    RENAME: 添加序号

---
+++ 0.0 Hello World.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+
+{
+    // Print "Hello, World!" to the console
+    printf("Hello, World!\n");
+    return 0; // Return 0 to indicate successful execution
+}
+ //DOEN
